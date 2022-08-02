e174b46a4c31d93cab4631b71aae6bc75a4e87cf
Ayred <ayredantara@gmail.com>
2022-08-01 20:41:21 -0700

    List of orders created.

--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,23 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
+    <v-col cols="12" sm="8" md="8">
       <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
+        <v-card-title class="headline"> Listado de órdenes </v-card-title>
+        <v-card-text
+          ><template>
+            <v-data-table
+              :headers="headers"
+              :items="orders"
+              class="elevation-1"
             >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
+              <template v-slot:items="props">
+                <td>{{ props.item.name }}</td>
+                <td class="text-xs-right">{{ props.item.number }}</td>
+                <td class="text-xs-right">{{ props.item.currency }}</td>
+                <td class="text-xs-right">{{ props.item.total }}</td>
+              </template>
+            </v-data-table>
+          </template>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
@@ -82,6 +30,45 @@
 
 <script>
 export default {
-  name: 'IndexPage',
+  data: () => ({
+    headers: [
+      {
+        text: 'No. Orden',
+        value: 'number',
+        align: 'start',
+      },
+      { text: 'Total', value: 'totals.total' },
+      {
+        text: 'Moneda',
+        align: 'start',
+        value: 'currency',
+      },
+    ],
+    orders: [],
+  }),
+  created() {
+    this.initialize()
+  },
+  methods: {
+    initialize() {
+      let authorization =
+        'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJwUGFINU55VXRxTUkzMDZtajdZVHdHV3JIZE81cWxmaCIsImlhdCI6MTYyMDY2Mjk4NjIwM30.lhfzSXW9_TC67SdDKyDbMOYiYsKuSk6bG6XDE1wz2OL4Tq0Og9NbLMhb0LUtmrgzfWiTrqAFfnPldd8QzWvgVQ'
+      this.orders = []
+      this.$axios
+        .$get('https://eshop-deve.herokuapp.com/api/v2/orders', {
+          headers: {
+            Authorization: `${authorization}`,
+          },
+        })
+        .then((res) => {
+          console.log(res)
+          if (res.success) {
+            this.orders = res.orders
+          } else {
+            console.log(this.orders)
+          }
+        })
+    },
+  },
 }
 </script>
